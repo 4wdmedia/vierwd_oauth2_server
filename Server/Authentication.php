dbb4b560b24cf8668db7ecdfb58e38669d65d8b0
Robert Vock <robertvock82@gmail.com>
2016-11-13 19:15:27 +0100

    Initial commit from vierwd_base

---
+++ Server/Authentication.php
@@ -0,0 +1,137 @@
+<?php
+namespace Vierwd\VierwdOAuth2Server\Server;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use League\OAuth2\Server\Exception\OAuthServerException;
+
+class Authentication extends \TYPO3\CMS\Sv\AbstractAuthenticationService {
+
+	public function oauthServer(ServerRequestInterface $request, ResponseInterface $response) {
+		$action = GeneralUtility::_GP('action');
+
+		if ($action === 'authorize') {
+			return $this->respondToAuthorize($request, $response);
+		}
+
+		if ($action === 'token') {
+			return $this->respondToAccessToken($request, $response);
+		}
+
+		if ($action === 'resource') {
+			return $this->respondToOwnerResource($request, $response);
+		}
+
+		// fsmir('Unknown action: ' . $action);
+	}
+
+	/**
+	 * @return League\OAuth2\Server\AuthorizationServer
+	 */
+	public function getOAuth2Server() {
+		$clientRepository = new OAuth2\Repositories\ClientRepository();
+		$scopeRepository = new OAuth2\Repositories\ScopeRepository();
+		$accessTokenRepository = new OAuth2\Repositories\AccessTokenRepository();
+		$authCodeRepository = new OAuth2\Repositories\AuthCodeRepository();
+		$refreshTokenRepository = new OAuth2\Repositories\RefreshTokenRepository();
+
+		$privateKey = GeneralUtility::getFileAbsFileName('EXT:vierwd_oauth2_server/Resources/Private/OAuth2/private.key');
+		$publicKey = GeneralUtility::getFileAbsFileName('EXT:vierwd_oauth2_server/Resources/Private/OAuth2/public.key');
+
+		// Setup the authorization server
+		$server = new \League\OAuth2\Server\AuthorizationServer(
+			$clientRepository,
+			$accessTokenRepository,
+			$scopeRepository,
+			$privateKey,
+			$publicKey
+		);
+		// Enable the client credentials grant on the server
+		// $server->enableGrantType(
+		// 	new \League\OAuth2\Server\Grant\ClientCredentialsGrant(),
+		// 	new \DateInterval('PT1H') // access tokens will expire after 1 hour
+		// );
+
+		// Enable the authentication code grant on the server with a token TTL of 1 hour
+		$server->enableGrantType(
+			new \League\OAuth2\Server\Grant\AuthCodeGrant(
+				$authCodeRepository,
+				$refreshTokenRepository,
+				new \DateInterval('PT10M')
+			),
+			new \DateInterval('PT1H')
+		);
+
+		return $server;
+	}
+
+
+	public function respondToAuthorize(ServerRequestInterface $request, ResponseInterface $response) {
+		$server = $this->getOAuth2Server();
+
+		try {
+			// Validate the HTTP request and return an AuthorizationRequest object.
+			// The auth request object can be serialized into a user's session
+			$authRequest = $server->validateAuthorizationRequest($request);
+			// Once the user has logged in set the user on the AuthorizationRequest
+			$authRequest->setUser(new OAuth2\Entities\UserEntity());
+			// Once the user has approved or denied the client update the status
+			// (true = approved, false = denied)
+			$authRequest->setAuthorizationApproved(true);
+			// Return the HTTP redirect response
+			return $server->completeAuthorizationRequest($authRequest, $response);
+		} catch (OAuthServerException $exception) {
+			// All instances of OAuthServerException can be formatted into a HTTP response
+
+			return $exception->generateHttpResponse($response);
+		} catch (\Exception $exception) {
+			// Unknown exception
+			$body = new Stream('php://temp', 'r+');
+			$body->write($exception->getMessage());
+			return $response->withStatus(500)->withBody($body);
+		}
+	}
+
+	public function respondToAccessToken(ServerRequestInterface $request, ResponseInterface $response) {
+		$server = $this->getOAuth2Server();
+
+		try {
+			return $server->respondToAccessTokenRequest($request, $response);
+		} catch (OAuthServerException $exception) {
+			return $exception->generateHttpResponse($response);
+		} catch (\Exception $exception) {
+			$body = new Stream('php://temp', 'r+');
+			$body->write($exception->getMessage());
+			return $response->withStatus(500)->withBody($body);
+		}
+	}
+
+	public function respondToOwnerResource(ServerRequestInterface $request, ResponseInterface $response) {
+		$accessTokenRepository = new OAuth2\Repositories\AccessTokenRepository();
+		$publicKey = GeneralUtility::getFileAbsFileName('EXT:vierwd_oauth2_server/Resources/Private/OAuth2/public.key');
+
+		$server = new \League\OAuth2\Server\ResourceServer($accessTokenRepository, $publicKey);
+
+		try {
+			if (!$request->hasHeader('authorization') && isset($_SERVER['Authorization'])) {
+				$request = $request->withAddedHeader('Authorization', $_SERVER['Authorization']);
+			}
+			$request = $server->validateAuthenticatedRequest($request);
+		} catch (OAuthServerException $exception) {
+			fsmir('incoming', $request, $response, $exception);
+			return $exception->generateHttpResponse($response);
+		} catch (\Exception $exception) {
+			return (new OAuthServerException($exception->getMessage(), 0, 'unknown_error', 500))
+				->generateHttpResponse($response);
+		}
+
+		$response->getBody()->write(json_encode([
+			'name' => 'rvock',
+			'realName' => 'Robert Vock',
+			'email' => 'robert.vock@4wdmedia.de',
+		]));
+		return $response->withStatus(200);
+	}
+}
---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "vierwd_oauth2_server"
+ *
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FORWARD MEDIA OAuth2 Server',
+	'description' => 'Adds OAuth2 Support for TYPO3',
+	'category' => 'TP6aEP3aca',
+	'author' => 'Robert Vock',
+	'author_email' => 'robert.vock@4wdmedia.de',
+	'author_company' => 'FORWARD MEDIA',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+			'vierwd_base' => '*',
+		),
+	),
+);
